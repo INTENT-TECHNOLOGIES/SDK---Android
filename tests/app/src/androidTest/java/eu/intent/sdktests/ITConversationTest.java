433364bfd006547bf5af148af21eae0c6676a249
Thomas Gallinari <thomas.gallinari@gmail.com>
2016-04-26 00:10:44 +0200

    Fix ITConversationTest according to changes in SDK v0.5

--- tests/app/src/androidTest/java/eu/intent/sdktests/ITConversationTest.java
+++ tests/app/src/androidTest/java/eu/intent/sdktests/ITConversationTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import eu.intent.sdk.api.ITRetrofitUtils;
+import eu.intent.sdk.model.ITAssetType;
 import eu.intent.sdk.model.ITConversation;
 import eu.intent.sdk.model.ITMessage;
 
@@ -21,13 +22,13 @@ public class ITConversationTest {
         String json = TestUtils.readRawFile(InstrumentationRegistry.getContext(), eu.intent.sdktests.test.R.raw.conversation);
         ITConversation conversation = ITRetrofitUtils.getGson().fromJson(json, ITConversation.class);
         assertEquals("04a17890-1122-48e0-95f4-3cc696a3bc4d", conversation.assetId);
-        assertEquals(ITConversation.AssetType.SITE, conversation.assetType);
+        assertEquals(ITAssetType.SITE, conversation.assetType);
         assertEquals("HEATING", conversation.activityCategory);
-        assertEquals(1457987228299l, conversation.creationDate);
+        assertEquals(1457987228299L, conversation.creationDate);
         assertEquals("Intent", conversation.creatorName);
-        assertEquals(1458896339111l, conversation.lastUpdate);
+        assertEquals(1458896339111L, conversation.lastUpdate);
         assertEquals("intent", conversation.creatorDomain);
-        assertEquals(0l, conversation.lastRead);
+        assertEquals(0L, conversation.lastRead);
         assertEquals(46, conversation.totalMessagesCount);
         assertEquals(4, conversation.messages.size());
         assertEquals("9004652e-59e5-4496-80a3-44a021a053a1", conversation.messages.get(0).id);
@@ -54,14 +55,14 @@ public class ITConversationTest {
         assertEquals("demo1.intent", conversation.messages.get(1).creatorDomain);
         assertEquals("demo1.intent", conversation.messages.get(2).creatorDomain);
         assertEquals("intent", conversation.messages.get(3).creatorDomain);
-        assertEquals(1458896339107l, conversation.messages.get(0).creationDate);
-        assertEquals(1458652668679l, conversation.messages.get(1).creationDate);
-        assertEquals(1458602226043l, conversation.messages.get(2).creationDate);
-        assertEquals(1457991858226l, conversation.messages.get(3).creationDate);
-        assertEquals(1458896339107l, conversation.messages.get(0).lastUpdate);
-        assertEquals(1458652668679l, conversation.messages.get(1).lastUpdate);
-        assertEquals(1458602237279l, conversation.messages.get(2).lastUpdate);
-        assertEquals(1457991858226l, conversation.messages.get(3).lastUpdate);
+        assertEquals(1458896339107L, conversation.messages.get(0).creationDate);
+        assertEquals(1458652668679L, conversation.messages.get(1).creationDate);
+        assertEquals(1458602226043L, conversation.messages.get(2).creationDate);
+        assertEquals(1457991858226L, conversation.messages.get(3).creationDate);
+        assertEquals(1458896339107L, conversation.messages.get(0).lastUpdate);
+        assertEquals(1458652668679L, conversation.messages.get(1).lastUpdate);
+        assertEquals(1458602237279L, conversation.messages.get(2).lastUpdate);
+        assertEquals(1457991858226L, conversation.messages.get(3).lastUpdate);
         assertEquals(false, conversation.messages.get(0).removed);
         assertEquals(false, conversation.messages.get(1).removed);
         assertEquals(false, conversation.messages.get(2).removed);
@@ -72,7 +73,7 @@ public class ITConversationTest {
         assertEquals(ITMessage.ReportingState.OPEN, conversation.messages.get(1).reportingState);
         assertEquals(1, conversation.messages.get(1).reportingStateHistory.size());
         assertEquals(ITMessage.ReportingState.OPEN, conversation.messages.get(1).reportingStateHistory.get(0).state);
-        assertEquals(1458652668679l, conversation.messages.get(1).reportingStateHistory.get(0).timestamp);
+        assertEquals(1458652668679L, conversation.messages.get(1).reportingStateHistory.get(0).timestamp);
         assertEquals("urn:demo1.intent:9879a242-78d6-489e-a17e-4434fc05ef50", conversation.messages.get(1).reportingStateHistory.get(0).changerUserId);
         assertEquals("Administrateur Demo", conversation.messages.get(1).reportingStateHistory.get(0).changerName);
         assertNull(conversation.messages.get(1).state);
@@ -80,8 +81,8 @@ public class ITConversationTest {
         assertEquals(2, conversation.messages.get(2).reportingStateHistory.size());
         assertEquals(ITMessage.ReportingState.OPEN, conversation.messages.get(2).reportingStateHistory.get(0).state);
         assertEquals(ITMessage.ReportingState.CLOSED, conversation.messages.get(2).reportingStateHistory.get(1).state);
-        assertEquals(1458602226043l, conversation.messages.get(2).reportingStateHistory.get(0).timestamp);
-        assertEquals(1458602235682l, conversation.messages.get(2).reportingStateHistory.get(1).timestamp);
+        assertEquals(1458602226043L, conversation.messages.get(2).reportingStateHistory.get(0).timestamp);
+        assertEquals(1458602235682L, conversation.messages.get(2).reportingStateHistory.get(1).timestamp);
         assertEquals("urn:demo1.intent:9879a242-78d6-489e-a17e-4434fc05ef50", conversation.messages.get(2).reportingStateHistory.get(0).changerUserId);
         assertEquals("urn:demo1.intent:9879a242-78d6-489e-a17e-4434fc05ef50", conversation.messages.get(2).reportingStateHistory.get(1).changerUserId);
         assertEquals("Administrateur Demo", conversation.messages.get(2).reportingStateHistory.get(0).changerName);
