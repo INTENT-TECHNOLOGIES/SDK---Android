1222c57a8178d4d969e1567aa4dd1ea23cfc819f
Thomas Gallinari <thomas.gallinari@gmail.com>
2017-01-03 18:25:42 +0100

    Add tickets search from API

--- sdk/android-sdk/src/main/java/eu/intent/sdk/util/ITJsonUtils.java
+++ sdk/android-sdk/src/main/java/eu/intent/sdk/util/ITJsonUtils.java
@@ -1,8 +1,12 @@
 package eu.intent.sdk.util;
 
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+
+import java.text.ParseException;
 
 /**
  * Provides some util methods to work with JSON.
@@ -18,5 +22,21 @@ public final class ITJsonUtils {
             result = json.get(key).getAsString();
         }
         return result;
+    }
+
+    public static long parseJsonIsoDate(@NonNull JsonObject jsonObject, @NonNull String jsonKey) {
+        long timestamp = 0;
+        JsonElement jsonDate = jsonObject.get(jsonKey);
+        if (jsonDate != null && !jsonDate.isJsonNull() && jsonDate.isJsonPrimitive()) {
+            try {
+                String firstEventDate = jsonDate.getAsString();
+                if (!TextUtils.isEmpty(firstEventDate)) {
+                    timestamp = ITDateUtils.parseDateIso8601(firstEventDate).getTime();
+                }
+            } catch (ClassCastException | ParseException ignored) {
+                // timestamp will be 0
+            }
+        }
+        return timestamp;
     }
 }
