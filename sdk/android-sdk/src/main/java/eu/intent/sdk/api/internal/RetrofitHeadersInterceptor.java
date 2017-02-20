808344f45831e51427d0075f8d079311814d8d6a
Thomas Gallinari <thomas.gallinari@gmail.com>
2017-02-20 12:20:38 +0100

    Add the app name and version as User-Agent in API calls

--- sdk/android-sdk/src/main/java/eu/intent/sdk/api/internal/RetrofitHeadersInterceptor.java
+++ sdk/android-sdk/src/main/java/eu/intent/sdk/api/internal/RetrofitHeadersInterceptor.java
@@ -1,6 +1,9 @@
 package eu.intent.sdk.api.internal;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
 
 import java.io.IOException;
 
@@ -16,21 +19,48 @@ public class RetrofitHeadersInterceptor implements Interceptor {
     private static final String TOKEN_ENDPOINT = "token";
 
     private Context mContext;
+    private String mUserAgent;
 
-    public RetrofitHeadersInterceptor(Context context) {
+    public RetrofitHeadersInterceptor(@NonNull Context context) {
         mContext = context;
+        mUserAgent = getUserAgent(context);
     }
 
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
-        // The only request that does not need headers is the token request
+        Request.Builder builder = request.newBuilder();
+        // The only request that does not need Authorization header is the token request
         if (!request.url().toString().endsWith(TOKEN_ENDPOINT)) {
-            // Use the current access token
-            // If the access token is not valid, the call will fail (401) and the Authenticator will be called
-            Oauth oauth = Oauth.getInstance(mContext);
-            request = request.newBuilder().header("Authorization", "Bearer " + oauth.getAccessToken()).build();
+            addAuthorizationHeader(builder);
         }
+        addUserAgentHeader(builder);
+        request = builder.build();
         return chain.proceed(request);
+    }
+
+    private void addAuthorizationHeader(@NonNull Request.Builder requestBuilder) {
+        // Use the current access token
+        // If the access token is not valid, the call will fail (401) and the Authenticator will be called
+        Oauth oauth = Oauth.getInstance(mContext);
+        requestBuilder.header("Authorization", "Bearer " + oauth.getAccessToken());
+    }
+
+    private void addUserAgentHeader(@NonNull Request.Builder requestBuilder) {
+        if (!TextUtils.isEmpty(mUserAgent)) {
+            requestBuilder.header("User-Agent", mUserAgent);
+        }
+    }
+
+    @NonNull
+    private String getUserAgent(@NonNull Context context) {
+        String appName = context.getString(context.getApplicationInfo().labelRes);
+        String appVersion;
+        try {
+            appVersion = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException ignored) {
+            appVersion = "";
+        }
+        return TextUtils.join(" ", new String[]{appName, appVersion}).trim();
     }
 }
