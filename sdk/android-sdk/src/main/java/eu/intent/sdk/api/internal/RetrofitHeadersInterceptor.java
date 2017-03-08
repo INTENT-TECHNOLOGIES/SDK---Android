1e8de7f8b173117d2795fefd3f42836c1e99c70d
Thomas Gallinari <thomas.gallinari@gmail.com>
2017-03-08 16:27:10 +0100

    Add User-Agent header in API calls

--- sdk/android-sdk/src/main/java/eu/intent/sdk/api/internal/RetrofitHeadersInterceptor.java
+++ sdk/android-sdk/src/main/java/eu/intent/sdk/api/internal/RetrofitHeadersInterceptor.java
@@ -1,9 +1,15 @@
 package eu.intent.sdk.api.internal;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import eu.intent.sdk.auth.internal.Oauth;
 import okhttp3.Interceptor;
@@ -17,10 +23,16 @@ import okhttp3.Response;
 public class RetrofitHeadersInterceptor implements Interceptor {
     private static final String TOKEN_ENDPOINT = "token";
 
+    private String mUserAgent;
     private Oauth mOauth;
 
-    public RetrofitHeadersInterceptor(@NonNull Oauth oauth) {
+    public RetrofitHeadersInterceptor(@NonNull Context context, @NonNull Oauth oauth) {
         mOauth = oauth;
+        try {
+            mUserAgent = getUserAgent(context);
+        } catch (PackageManager.NameNotFoundException e) {
+            mUserAgent = "";
+        }
     }
 
     @Override
@@ -31,6 +43,7 @@ public class RetrofitHeadersInterceptor implements Interceptor {
         if (!request.url().toString().endsWith(TOKEN_ENDPOINT)) {
             addAuthorizationHeader(builder);
         }
+        addUserAgentHeader(builder);
         request = builder.build();
         return chain.proceed(request);
     }
@@ -39,5 +52,24 @@ public class RetrofitHeadersInterceptor implements Interceptor {
         // Use the current access token
         // If the access token is not valid, the call will fail (401) and the Authenticator will be called
         requestBuilder.header("Authorization", "Bearer " + mOauth.getAccessToken());
+    }
+
+    private void addUserAgentHeader(@NonNull Request.Builder requestBuilder) {
+        if (!TextUtils.isEmpty(mUserAgent)) {
+            try {
+                requestBuilder.header("User-Agent", URLEncoder.encode(mUserAgent, "ISO-8859-1"));
+            } catch (UnsupportedEncodingException ignored) {
+                // Don't add the User-Agent
+            }
+        }
+    }
+
+    @NonNull
+    private String getUserAgent(@NonNull Context context) throws PackageManager.NameNotFoundException {
+        PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+        Resources res = context.getResources();
+        String appName = res.getString(packageInfo.applicationInfo.labelRes);
+        String appVersion = packageInfo.versionName;
+        return TextUtils.join(" ", new String[]{appName, appVersion}).trim();
     }
 }
